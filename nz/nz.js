55b83ff20ff50674bddbbc428fc9025b36ddcec6
Reverse <1308168694@qq.com>
2022-08-07 00:36:49 +0800

    Update nz.js

--- nz/nz.js
+++ nz/nz.js
@@ -1,10 +1,8 @@
 //Logo和版权
-<script>
-    window.onload = function(){
-        var avatar=document.querySelector(".item img")
-        var footer=document.querySelector("div.is-size-7")
-        footer.style.visibility="visible"
-        avatar.src="https://pic.imgdb.cn/item/624aa2d5239250f7c5147559.png"
-        avatar.style.visibility="visible"
-    }
-</script>
+window.onload = function () {
+    var avatar = document.querySelector(".item img")
+    var footer = document.querySelector("div.is-size-7")
+    footer.style.visibility = "visible"
+    avatar.src = "https://pic.imgdb.cn/item/624aa2d5239250f7c5147559.png"
+    avatar.style.visibility = "visible"
+}
